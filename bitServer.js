79d00fb110aad025d0649ebf6f14f5aaa1a511f7
jungyk <new1234@cmesoft.co.kr>
2020-01-30 13:53:01 +0900

    last commit 20200130

--- bitServer.js
+++ bitServer.js
@@ -1,5 +1,9 @@
-const express = require('express');
-const app = express();
+const express       = require('express');
+const bodyParser    = require('body-parser');
+const app           = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const server = app.listen(3300, () => {
     console.log("Express server has started on port 3300");
